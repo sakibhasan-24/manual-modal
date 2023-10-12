7917151e339d975c09c180ef72ef25254c287923
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-12 13:40:33 +0600

    add a simple function for avoiding repeation of code

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-icon");
 
 offerBtn.addEventListener("click", function () {
-  mainContainer.classList.add("active");
-  modalContainer.classList.remove("active");
+  /* mainContainer.classList.add("active");
+  modalContainer.classList.remove("active"); */
+  styleAdd(mainContainer, "active");
+  styleRemove(modalContainer, "active");
 });
 closeBtn.addEventListener("click", function () {
-  mainContainer.classList.remove("active");
-  modalContainer.classList.add("active");
+  //   mainContainer.classList.remove("active");
+  //   modalContainer.classList.add("active");
+  styleRemove(mainContainer, "active");
+  styleAdd(modalContainer, "active");
 });
+function styleAdd(className, style) {
+  className.classList.add(style);
+}
+function styleRemove(className, style) {
+  className.classList.remove(style);
+}
